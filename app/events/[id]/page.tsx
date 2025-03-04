b7ec3dd3934d14760774a4b76cc51e52e7da639e
Jean Ceugniet <jean.ceugniet@gmail.com>
2025-03-04 21:57:56 +0100

    Added events list page Added EventsList component Added EventItem component Added event details page Seed.ts : minor change

---
+++ app/events/[id]/page.tsx
@@ -0,0 +1,28 @@
+import React from "react";
+import { PrismaClient, Event } from "@prisma/client";
+
+const prisma = new PrismaClient();
+
+const getEventById = async (id: string): Promise<Event | null> => {
+    return await prisma.event.findUnique({
+        where: { id },
+    });
+};
+
+export default async function EventDetailPage({ params }: { params: { id: string } }) {
+    const { id } = await params
+    const event = await getEventById(id);
+
+    if (!event) return <p>Event not found</p>;
+
+    return (
+        <div className="p-6">
+            <h1 className="text-3xl font-bold">{event.name}</h1>
+            <p>{event.desc}</p>
+            <img src={event.picture} alt={event.name} className="w-full max-w-lg my-4" />
+            <p className="text-gray-500">
+                {new Date(event.start_at).toLocaleString()} - {new Date(event.end_at).toLocaleString()}
+            </p>
+        </div>
+    );
+}
---
+++ app/events/page.tsx
@@ -0,0 +1,24 @@
+import React from "react";
+import EventsList from "../components/EventsList";
+import { PrismaClient, Event } from "@prisma/client";
+
+const prisma = new PrismaClient();
+
+
+const getEvents = async (): Promise<Event[]> => {
+    const events = await prisma.event.findMany();
+    console.log(events)
+
+    return events
+};
+
+export default async function EventsPage() {
+    const events: Event[] = await getEvents();
+
+    return (
+        <div className="p-6">
+            <h1 className="text-3xl font-bold mb-4">Events</h1>
+            <EventsList events={events} />
+        </div>
+    );
+}
